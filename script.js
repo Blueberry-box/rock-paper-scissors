88f006d1b19217000bd52ca67aae454222fcf24a
Blueberry-box <245140791+Blueberry-box@users.noreply.github.com>
2025-12-28 06:09:06 +0200

    add playGame

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log("Hello World");
 
+
 function getComputerChoice () {
     let randomNumber = Math.random();
     if (randomNumber < 1/3) {
@@ -10,3 +11,49 @@ function getComputerChoice () {
         return "scissors";
     }
 }
+
+function getHumanChoice () {
+    let choice = prompt("Pick Rock, Paper or Scissors","");
+    return choice;
+}
+
+function playRound (humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log(`Computer: ${computerChoice}. Human: ${humanChoice}. It's a tie!`);
+        return;
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log(`Computer: ${computerChoice}. Human: ${humanChoice}. Computer wins!`);
+        computerScore++;
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log(`Computer: ${computerChoice}. Human: ${humanChoice}. Human wins!`);
+        humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log(`Computer: ${computerChoice}. Human: ${humanChoice}. Human wins!`);
+        humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log(`Computer: ${computerChoice}. Human: ${humanChoice}. Computer wins!`);
+        computerScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log(`Computer: ${computerChoice}. Human: ${humanChoice}. Computer wins!`);
+        computerScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("Human wins!")
+        humanScore++;
+    }
+}
+
+
+
+function playGame () {
+    const humanChoice = getHumanChoice().toLowerCase();
+    const computerChoice = getComputerChoice();
+
+    let humanScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        playRound(humanChoice, computerChoice);
+    }
+    console.log(`Human score:${humanScore} Computer score: ${computerScore}`);
+}
+
+playGame();
