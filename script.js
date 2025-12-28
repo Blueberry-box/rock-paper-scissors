e6f91009c2fc6f0b630b70808505f119385cfc16
Blueberry-box <245140791+Blueberry-box@users.noreply.github.com>
2025-12-28 04:33:24 +0200

    add getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Hello World");
+
+function getComputerChoice () {
+    let randomNumber = Math.random();
+    if (randomNumber < 1/3) {
+        return "rock";
+    } else if (randomNumber < 2/3) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
